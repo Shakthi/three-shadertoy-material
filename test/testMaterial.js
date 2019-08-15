215db59ca9db3719114cb646264775399968ea59
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2019-08-15 12:29:10 +0530

    Start work on buffers

--- test/testMaterial.js
+++ test/testMaterial.js
@@ -1,11 +1,11 @@
 
 var THREE = require("three");
 var ShaderToyMaterial  = require("../src/ShaderToyMaterial").default;
-var sampleShader  = require("./sample.txt");
+// var sampleShader  = require("./sample.txt");
 
 var camera, scene, renderer;
 var geometry, material, mesh;
- 
+ var texture;
 init();
 animate();
  
@@ -17,11 +17,23 @@ function init() {
     scene = new THREE.Scene();
  
     geometry = new THREE.BoxGeometry( 0.5, 0.5, 0.5 );
+    texture = new THREE.TextureLoader().load( 'test/52d2a8f514c4fd2d9866587f4d7b2a5bfa1a11a0e772077d7682deb8b3b517e5.jpg' );
     material = new ShaderToyMaterial(
-        `void mainImage( out vec4 O,  vec2 U ){
-            U = 2.* sin (25.*U/iResolution.x);  
-            O = .5 + .5* sin( U.x+U.y + vec4(0,2.4,-2.4,0) +iTime);
-        }`
+        `void mainImage( out vec4 fragColor, in vec2 fragCoord )
+        {
+            vec2 uv = fragCoord.xy / iResolution.xy;
+        //	uv.y = -uv.y;
+            
+            uv.y += (cos((uv.y + (iTime * 0.04)) * 45.0) * 0.0019) +
+                (cos((uv.y + (iTime * 0.1)) * 10.0) * 0.002);
+        
+            uv.x += (sin((uv.y + (iTime * 0.07)) * 15.0) * 0.0029) +
+                (sin((uv.y + (iTime * 0.1)) * 15.0) * 0.002);
+                
+        
+            vec4 texColor = texture(iChannel0,uv);
+            fragColor = texColor;
+        }`,{map:texture}
     ); 
     mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
@@ -35,10 +47,10 @@ function init() {
 function animate() {
  
     requestAnimationFrame( animate );
- 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
- 
+` 
+    // mesh.rotation.x += 0.01;
+    // mesh.rotation.y += 0.02;
+` 
     renderer.render( scene, camera );
  
 }
